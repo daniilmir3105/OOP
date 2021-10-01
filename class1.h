dd28aa5409c1b294cc0e3be6ca141100da52c0ca
Daniil <miroshnichienko.daniil@gmail.com>
2021-10-01 20:04:09 +0300

    define new classes

--- class1.h
+++ class1.h
@@ -5,4 +5,34 @@
 #ifndef OOP_CLASS1_H
 #define OOP_CLASS1_H
 
+//#include "sfuns.h"
+#include <stdlib.h>
+
+// define class
+struct class1;
+typedef struct class1 t_class1;
+
+// define events in class
+typedef struct t_class1_events {
+    void (*on_event_1)(t_class1* object);
+    void (*on_event_2)(t_class1* object);
+    void (*on_event_3)(t_class1* object);
+    void (*on_event_4)(t_class1* object);
+    void (*on_event_5)(t_class1* object);
+} t_class1_events;
+
+// constructor and destructor
+t_class1 * class1_create (int* exception);
+void class1_destroy(t_class1* object);
+
+// define event listener
+void class1_set_listener(t_class1* object, t_class1_events* listener);
+
+// memory of the ancestors
+void class1_set_parent(t_class1* object, void* parent);
+void* class1_get_parent(t_class1* object);
+
+void class1_set_value(t_class1* object, void* value);
+void* class1_get_value(t_class1* object);
+
 #endif //OOP_CLASS1_H
--- class2.h
+++ class2.h
@@ -5,4 +5,34 @@
 #ifndef OOP_CLASS2_H
 #define OOP_CLASS2_H
 
+//#include "sfuns.h"
+#include <stdlib.h>
+
+// define class
+struct class2;
+typedef struct class2 t_class2;
+
+// define events in class
+typedef struct myclass_events {
+    void (*on_event_1)(t_class2* object);
+    void (*on_event_2)(t_class2* object);
+    void (*on_event_3)(t_class2* object);
+    void (*on_event_4)(t_class2* object);
+    void (*on_event_5)(t_class2* object);
+} t_class2_events;
+
+// constructor and destructor
+t_class2 * class2_create (int* exception);
+void class2_destroy(t_class2* object);
+
+// define event listener
+void class2_set_listener(t_class2* object, t_class2_events* listener);
+
+// memory of the ancestors
+void class2_set_parent(t_class2* object, void* parent);
+void* class2_get_parent(t_class2* object);
+
+void class2_set_value(t_class2* object, void* value);
+void* class2_get_value(t_class2* object);
+
 #endif //OOP_CLASS2_H
